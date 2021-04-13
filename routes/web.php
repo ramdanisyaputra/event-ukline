17a7f182b65e7d41da85f05bd1310aa7f9965f3e
aboutmf <mf.about@gmail.com>
2021-04-13 09:37:19 +0700

    templating

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Superadmin\GradeController;
 use App\Http\Controllers\Superadmin\ProvinceController;
 use App\Http\Controllers\Superadmin\RegencyController;
 use App\Http\Controllers\Superadmin\SchoolAdminController;
+use App\Http\Controllers\Superadmin\SuperadminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,36 +30,39 @@ Route::get('/login',[AuthController::class,'loginForm'])->name('loginForm');
 Route::post('/login',[AuthController::class,'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::prefix('provinces')->name('provinces.')->group(function(){
-    Route::get('', [ProvinceController::class,'index'])->name('index');
-    Route::post('store', [ProvinceController::class,'store'])->name('store');
-    Route::post('update', [ProvinceController::class,'update'])->name('update');
-});
-Route::prefix('regencies')->name('regencies.')->group(function(){
-    Route::get('', [RegencyController::class,'index'])->name('index');
-    Route::post('store', [RegencyController::class,'store'])->name('store');
-    Route::post('update', [RegencyController::class,'update'])->name('update');
-});
-Route::prefix('education-levels')->name('education-levels.')->group(function(){
-    Route::get('', [EducationLevelController::class,'index'])->name('index');
-    Route::post('store', [EducationLevelController::class,'store'])->name('store');
-    Route::post('update', [EducationLevelController::class,'update'])->name('update');
-});
-Route::prefix('grades')->name('grades.')->group(function(){
-    Route::get('', [GradeController::class,'index'])->name('index');
-    Route::post('store', [GradeController::class,'store'])->name('store');
-    Route::post('update', [GradeController::class,'update'])->name('update');
+Route::prefix('superadmin')->name('superadmin.')->group(function() {
+    Route::get('/', [SuperadminController::class, 'index'])->name('index');
+    Route::prefix('provinces')->name('provinces.')->group(function(){
+        Route::get('', [ProvinceController::class,'index'])->name('index');
+        Route::post('store', [ProvinceController::class,'store'])->name('store');
+        Route::post('update', [ProvinceController::class,'update'])->name('update');
+    });
+    Route::prefix('regencies')->name('regencies.')->group(function(){
+        Route::get('', [RegencyController::class,'index'])->name('index');
+        Route::post('store', [RegencyController::class,'store'])->name('store');
+        Route::post('update', [RegencyController::class,'update'])->name('update');
+    });
+    Route::prefix('education-levels')->name('education-levels.')->group(function(){
+        Route::get('', [EducationLevelController::class,'index'])->name('index');
+        Route::post('store', [EducationLevelController::class,'store'])->name('store');
+        Route::post('update', [EducationLevelController::class,'update'])->name('update');
+    });
+    Route::prefix('grades')->name('grades.')->group(function(){
+        Route::get('', [GradeController::class,'index'])->name('index');
+        Route::post('store', [GradeController::class,'store'])->name('store');
+        Route::post('update', [GradeController::class,'update'])->name('update');
+    });
+    Route::prefix('school-admins')->name('school-admins.')->group(function(){
+        Route::get('', [SchoolAdminController::class,'index'])->name('index');
+        Route::post('store', [SchoolAdminController::class,'store'])->name('store');
+        Route::post('update', [SchoolAdminController::class,'update'])->name('update');
+    });
+    Route::prefix('tags')->name('tags.')->group(function(){
+        Route::get('', [FaqController::class,'index'])->name('index');
+        Route::post('store', [FaqController::class,'store'])->name('store');
+        Route::post('update', [FaqController::class,'update'])->name('update');
+    });
 });
 
-Route::prefix('school-admins')->name('school-admins.')->group(function(){
-    Route::get('', [SchoolAdminController::class,'index'])->name('index');
-    Route::post('store', [SchoolAdminController::class,'store'])->name('store');
-    Route::post('update', [SchoolAdminController::class,'update'])->name('update');
-});
-Route::prefix('tags')->name('tags.')->group(function(){
-    Route::get('', [FaqController::class,'index'])->name('index');
-    Route::post('store', [FaqController::class,'store'])->name('store');
-    Route::post('update', [FaqController::class,'update'])->name('update');
-});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

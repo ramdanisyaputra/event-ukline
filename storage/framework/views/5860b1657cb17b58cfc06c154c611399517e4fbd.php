0a6c6308cfd8f228d31c45006d317dceab30696b
ramdanisyaputra <rmdn.syaputra@gmail.com>
2021-04-14 14:48:05 +0700

    import students

---
+++ storage/framework/views/5860b1657cb17b58cfc06c154c611399517e4fbd.php
@@ -0,0 +1,23 @@
+<ul class="sidebar-menu">
+    <li class="menu-header">Beranda</li>
+    <li class="nav-item <?php echo e(request()->is('school_admin') ? 'active' : ''); ?>">
+        <a href="<?php echo e(route('school_admin.index')); ?>" class="nav-link"><i class="fas fa-fire"></i><span>Beranda</span></a>
+    </li>
+    <li class="menu-header">Data Utama</li>
+    <li class="nav-item <?php echo e(request()->is('school_admin/subjects*') ? 'active' : ''); ?>">
+        <a href="<?php echo e(route('school_admin.subjects.index')); ?>" class="nav-link"><i class="fas fa-book"></i><span>Matapelajaran</span></a>
+    </li>
+    <li class="nav-item <?php echo e(request()->is('school_admin/classes*') ? 'active' : ''); ?>">
+        <a href="<?php echo e(route('school_admin.classes.index')); ?>" class="nav-link"><i class="fas fa-university"></i><span>Data Kelas</span></a>
+    </li>
+    <li class="menu-header">Data Lainnya</li>
+    <li class="nav-item <?php echo e(request()->is('school_admin/students*') ? 'active' : ''); ?>">
+        <a href="<?php echo e(route('school_admin.students.index')); ?>" class="nav-link"><i class="fas fa-user"></i><span>Data Siswa</span></a>
+    </li>
+    <li class="nav-item <?php echo e(request()->is('school_admin/exams*') ? 'active' : ''); ?>">
+        <a href="<?php echo e(route('school_admin.exams.index')); ?>" class="nav-link"><i class="fas fa-laptop"></i><span>Kumpulan Ujian</span></a>
+    </li>
+    <li class="nav-item <?php echo e(request()->is('school_admin/exam-types*') ? 'active' : ''); ?>">
+        <a href="" class="nav-link"><i class="fas fa-newspaper "></i><span>Nilai Ujian</span></a>
+    </li>
+</ul><?php /**PATH D:\xampp\htdocs\event-ukline\resources\views/components/_sidebar_school_admin.blade.php ENDPATH**/ ?>
